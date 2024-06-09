8b20a99798c755398a360ab39cb6e5ef58fe32eb
Almonzer Osman <almonzer.m.osman@gmail.com>
2024-06-09 18:01:18 +0400

    Add colors constant
    Signed-off-by: Almonzer Osman <almonzer.m.osman@gmail.com>

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF27605F);
+const Color lightGreen = Color(0xFFFFFFCB);
+const Color lightOrange = Color(0xFFFCEEE5);
+const Color darkOrange = Color(0xFFA8424C);
+const Color bgColor = Color(0xFFFCFAF6);
+const Color darkGrey = Color(0xFFDFE3E4);
+const Color lightGrey = Color(0xFFF4F6F6);
+const Color white = Color(0xFFFFFFFF);
